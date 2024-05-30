24a47211aaafd54ab90854f7456081dfa1557dbd
Kens04 <kento090403@icloud.com>
2024-05-30 22:52:37 +0900

    zodResolverを導入してスキーマの型定義を適用

--- src/components/MailForm/MailForm.tsx
+++ src/components/MailForm/MailForm.tsx
@@ -1,13 +1,32 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
-import { useForm } from 'react-hook-form'
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "@/lib/formSchema";
+import { Textarea } from "../ui/textarea";
 
 const MailForm = () => {
-  const form = useForm();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      subject: "",
+      email: "",
+      content: "",
+    },
+  });
 
   function onSubmit(values: any) {
     console.log(values);
@@ -15,27 +34,68 @@ const MailForm = () => {
 
   return (
     <Form {...form}>
-    <form onSubmit={() => {form.handleSubmit(onSubmit)}} className="container flex flex-col gap-3">
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
-  </Form>
-  )
-}
+      <form
+        onSubmit={() => {
+          form.handleSubmit(onSubmit);
+        }}
+        className="container flex flex-col gap-3"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ユーザー名</FormLabel>
+              <FormControl>
+                <Input placeholder="ユーザー名" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input placeholder="メールアドレス" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>主題</FormLabel>
+              <FormControl>
+                <Input placeholder="主題" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>本文</FormLabel>
+              <FormControl>
+                <Textarea placeholder="本文" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
 
-export default MailForm
+export default MailForm;
