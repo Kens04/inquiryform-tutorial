3ff699f9e2eb3c5346e6fcd4ad4a68e30b66c72a
Kens04 <kento090403@icloud.com>
2024-05-31 23:52:23 +0900

    送信時にローディング状態を実装しよう

--- src/components/MailForm/MailForm.tsx
+++ src/components/MailForm/MailForm.tsx
@@ -13,6 +13,7 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { useMailForm } from "@/hooks/useMailForm";
+import { ClipLoader } from "react-spinners";
 
 const MailForm = () => {
   const { form, onSubmit } = useMailForm();
@@ -96,7 +97,9 @@ const MailForm = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" disabled={form.formState.isSubmitting}>
+          {form.formState.isSubmitting ? <ClipLoader /> : "送信"}
+        </Button>
       </form>
     </Form>
   );
