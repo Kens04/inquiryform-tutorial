66d37d7d51218f8512ee348311f86b1afff14291
Kens04 <kento090403@icloud.com>
2024-05-31 23:18:17 +0900

    ファイルデータを文字列と一緒に送信するにはどうすれば良いのか

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-  const {username, subject, email, content} = await request.json();
+  const { username, subject, email, content, file } = await request.json();
 
   try {
     const { data, error } = await resend.emails.send({
@@ -17,6 +17,7 @@ export async function POST(request: Request) {
         email: email,
         content: content,
       }) as React.ReactElement,
+      attachments: [{ filename: file.name, content: file }],
     });
     if (error) {
       return NextResponse.json({ error });
