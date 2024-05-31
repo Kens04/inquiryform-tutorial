99d853cbcb88403476648fa6f29c7ba9b5cbe96f
Kens04 <kento090403@icloud.com>
2024-05-31 22:35:00 +0900

    Gmail送信API

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,27 @@
+import { EmailTemplate } from "@/components/email-template";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["kentoninnzya0403@gmail.com"],
+      subject: "フォーム開発相談",
+      react: EmailTemplate({
+        username: "testUser",
+        email: "test@gmail.com",
+        content: "フォーム開発のご相談です。",
+      }) as React.ReactElement,
+    });
+    if (error) {
+      return NextResponse.json({ error });
+    }
+
+    return NextResponse.json({ data });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
